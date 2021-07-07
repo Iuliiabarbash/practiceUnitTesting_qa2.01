9fed21e5e1430bb9b1c4c039a79d09da274fcb1c
Yulia <iuliiabarbash@gmail.com>
2021-07-07 17:54:38 -0400

    another task is completed

--- assertionTesting.test.ts
+++ assertionTesting.test.ts
@@ -5,10 +5,16 @@ describe("testing assertions", () => {
     const bigger: number = 321654987;
 
     // Add an assertion that "smaller" is less than "bigger"
+    expect(smaller).toBeLessThan(bigger);
+    expect(smaller < bigger).toBeTruthy();
 
     // Add another assertion that "greaterOrEqual" is greater than or equal to "smaller"
+    expect(greaterOrEqual).toBeGreaterThanOrEqual(smaller);
+    expect(greaterOrEqual >= smaller).toBeTruthy();
 
     // Add a final assertion that "bigger" is *not* less than "smaller"
+    expect(bigger).not.toBeLessThan(smaller);
+
   });
   it("asserts on truth", () => {
     const hello: string = "Hello World";
@@ -17,7 +23,10 @@ describe("testing assertions", () => {
     // One of these statements is true, and one is not. Wrap each in the appropriate assertion!
     hello.includes("Hi There");
     sentence.includes("fox");
+    expect(hello.includes("Hi There")).toBeFalsy();
+    expect(sentence.includes("fox")).toBeTruthy();
   });
+
   it("asserts on things being the same", () => {
     const numberCompare1: number = 3.14159;
     const numberCompare2: number = 3.14;
@@ -25,7 +34,10 @@ describe("testing assertions", () => {
     const stringCompare2: string = "Hi";
 
     // Add a passing assertion comparing numberCompare1 and numberCompare2 based on being equal
+    expect(numberCompare1).not.toEqual(numberCompare2);
+    expect(stringCompare1).toEqual(stringCompare2);
 
     // Add a passing assertion comparing stringCompare1 and stringCompare2 based on being equal
+  
   });
 });
